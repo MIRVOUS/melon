8025722437627339b4a1e4490381c8d28d91ddeb
Dandy <dandy.euis01@gmail.com>
2019-07-31 13:46:45 +0700

    d"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Melon Team
+                    "Melon Team"
                 </div>
 
                 <div class="links">
