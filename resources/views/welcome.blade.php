998d9ce034a33a20509d3afe1c34da53bfc2a03c
Mahesa <mahesaramdani11@gmail.com>
2019-07-31 13:38:01 +0700

    team1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Melon
+                    Melon Team
                 </div>
 
                 <div class="links">
